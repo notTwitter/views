04628811a8f01d3d993ac9d7e815d71c346d5ec5
Thenu Kaluarachchi <thenujaya123@gmail.com>
2022-05-28 12:05:05 +0530

    Added basic functionality to test server

--- testServer/server.js
+++ testServer/server.js
@@ -6,8 +6,23 @@ const PORT = 80
 const app = express()
 app.use(cors())
 
+app.use(session({
+    secret: 'Super secret secret',
+    resave: false,      
+    saveUninitialized: false,     
+    cookie:{
+        maxAge: null
+    }
+}))
+
 app.listen(80, ()=> {console.log(`Test Server runninng on port ${PORT}`)})
 
 app.get('/test/login', async(req, res)=>{
-    res.send("Hello World")
+    req.session.loggedIn = true
+    res.send("Logged In")
+})
+
+app.get('/test/checkAuth', async(req, res)=>{
+    console.log(req.session.loggedIn)
+    res.send(req.session.loggedIn)
 })
