4043d7638aa463d34bf009da7d8ba71cb0b4f821
Thenu Kaluarachchi <thenujaya123@gmail.com>
2022-05-28 13:49:39 +0530

    Temp fix for CORs error

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -3,14 +3,15 @@ import { NextResponse, NextRequest } from "next/server";
 export async function middleware(req, ev) {
   const { pathname, origin } = req.nextUrl;
   if (pathname == "/home") {
-    
     //Checking the user's session
-    console.log("========")
+    console.log("========");
     let sessionCookie = req.cookies["connect.sid"];
-    console.log(sessionCookie)
-    fetch('http://localhost:80/test/checkAuth', {credentials: 'include'} )
-    .then(res => {return res.json()})
-    .then(data => console.log(data))
+    console.log(sessionCookie);
+    fetch("http://localhost:80/test/checkAuth", { credentials:'include' })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => console.log(data));
 
     //NextJS middleware does not support relative URLs, so the origin URL must be infered.
     return NextResponse.redirect(`${origin}/`);
--- testServer/server.js
+++ testServer/server.js
@@ -1,30 +1,41 @@
-const express = require('express')
-const session = require('express-session')
-const cors = require('cors')
-const PORT = 80
+const express = require("express");
+const session = require("express-session");
+const cors = require("cors");
+const PORT = 80;
 
-const app = express()
-app.use(cors())
-app.use(session({
-    secret: 'Super secret secret',
-    resave: false,      
-    saveUninitialized: false,     
-    cookie:{
-        maxAge: null
-    }
-}))
+const app = express();
 
-app.listen(80, ()=> {console.log(`Test Server runninng on port ${PORT}`)})
+app.listen(80, () => {
+    console.log(`Test Server runninng on port ${PORT}`);
+  });
+  
+app.use(cors({
+    //Temporary fix
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
+app.use(
+  session({
+    secret: "Super secret secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: null,
+    },
+  })
+);
 
-app.get('/test/login', async(req, res)=>{
-    console.log("Requested login")
-    req.session.loggedIn = true
-    console.log("Logged In")
-    res.send("Logged In")
-})
+app.get("/test/login", async (req, res) => {
+  console.log("Requested login");
+  req.session.loggedIn = true;
+  console.log("Logged In");
+  res.send("Logged In");
+});
 
-app.get('/test/checkAuth', async(req, res)=>{
-    console.log("Requested checkAuth")
-    const isLoggedIn = req.session.loggedIn || false
-    res.send({"isLoggedIn": isLoggedIn})
-})
+app.get("/test/checkAuth", async (req, res) => {
+  console.log(req.cookies)
+  console.log("Requested checkAuth");
+  const isLoggedIn = req.session.loggedIn || false;
+  console.log(isLoggedIn);
+  res.send({ isLoggedIn: isLoggedIn });
+});
