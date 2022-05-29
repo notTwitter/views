e6d147e236520bebcd304fe72b2ec13414eb3dcd
Thenu Kaluarachchi <thenujaya123@gmail.com>
2022-05-30 00:42:36 +0530

    Removed tests

--- src/Layout/HomeLayout/HomeLayout.tsx
+++ src/Layout/HomeLayout/HomeLayout.tsx
@@ -11,31 +11,33 @@ const HomeLayout = () => {
   console.log("Render!")
 
   /* Javascript media query to check whether the screen is mobile dimensions
+
   *       Using a **toggle** inside of the query function as the function is called **whenever** the screen size 
   *  moves through the mobile width border; i.e, both up and down.
   *  The listener will only be called on the first render. There is no need to re-assign an event listener each time
   *  because the event listener will not be consulting the current state. There is no need to worry about 'state freeze' (definition inside /retconned/sideMenu.md)
+  * 
+  *       Will have to set a definitive value for isMobile on the very first render,
+  *  so will be creating the reducer to accept a toggle & a definitive set in state.
   */
   useEffect(() => {
     const toggleMobile = ():void=> {
-      console.log("Yo")
+      // toggle isMobile
     }
     const mobileCheck = window.matchMedia(`(max-width:${MOBILE_BREAK_POINT}px)`)
     mobileCheck.addEventListener('change', toggleMobile)
     
     //Manually checking the matchMedia only for the **first** render
     if(mobileCheck.matches===true){
-      console.log("Yo from first render")
+     // isMobile = true
     }
     return ()=> {
       mobileCheck.removeEventListener('change', toggleMobile)
-      console.log("Removed")
     }
   }, [])
  
   return (
     <>
-    <button style={{height: '50px', width: '50px'}} onClick={()=>{setCount(count+1)}}>asdfadf</button>
     <S.MainSection id="main">
         <SideMenu />
         <S.OutletContainer>
