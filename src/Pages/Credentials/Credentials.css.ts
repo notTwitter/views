092688e8562d4f941d6202ec9c9b4785389468c7
Thenu Kaluarachchi <thenujaya123@gmail.com>
2022-05-29 23:49:58 +0530

    Basic credentials setup

---
+++ src/Pages/Credentials/Credentials.css.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const MainSection = styled.section`
+    width: 100%; 
+    /* height: max(100vh, 500px); */
+    display: flex;
+    position: relative;
+`
+
+export const ImageSection = styled.section`
+    width: 40%;
+    height: 100%;
+`
+
+export const ContentSection = styled.section`
+    flex-grow: 1;
+    height: 100%;
+`
--- src/Pages/Credentials/Credentials.tsx
+++ src/Pages/Credentials/Credentials.tsx
@@ -2,6 +2,7 @@ import { FC, useState } from "react";
 import Login from "./Login/Login";
 import Register from './Register/Register'
 import { Link } from "react-router-dom";
+import * as S from './Credentials.css'
 
 //Types
 type CredentialsPropType = {
@@ -11,13 +12,16 @@ type CredentialsPropType = {
 const Credentials:FC<CredentialsPropType> = (props) => {
 
   return (
-    <>
-      {
+    <S.MainSection>
+      <S.ImageSection></S.ImageSection>
+      <S.ContentSection>
+        {
         props.isLogin
           ?   <Login/>
           :   <Register/>
-      }
-    </>
+        }
+      </S.ContentSection>
+    </S.MainSection>
   );
 };
 
