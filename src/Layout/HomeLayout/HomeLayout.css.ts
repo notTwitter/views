02077c3a68b01c6dc01225e2aa0e1bb6eefb1c56
Thenu Kaluarachchi <thenujaya123@gmail.com>
2022-05-29 23:49:24 +0530

    Found reason for outlet being pushed right

--- src/Layout/HomeLayout/HomeLayout.css.ts
+++ src/Layout/HomeLayout/HomeLayout.css.ts
@@ -13,7 +13,7 @@ export const MainSection = styled.section`
 
 export const OutletContainer = styled.div`
   flex-grow: 1;
-  z-index: 100;
+  z-index: 99;
   height: 50000px;
 `;
 
@@ -23,6 +23,11 @@ export const OutletContainer = styled.div`
 * the outlet container is pushed to the side anyways. I'm not sure whether this is standard behaviour and whether this will work across all
 * browsers. I thought it might have something to do with both their z-indexes being the same, but after tinkering with it for a bit, the z-index does not appear 
 * to be the reason. I left the z-indexes anyhow.
+*
+* SOLVED!---------------------------------------
+*       The thing that was making the outlet container be pushed to the right even though the sidemenu was sticky, 
+* was the fact that the MainSection container had a display type of flex. Remove the flex, and the the outlet container no longer
+* gets pushed to the right.
 */
 
 /* To-Do!
