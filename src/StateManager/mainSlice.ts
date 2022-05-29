cd5b9bf85877c95298c7c1bb0e58290629673c8b
Thenu Kaluarachchi <thenujaya123@gmail.com>
2022-05-30 01:12:03 +0530

    Setup redux state for isMobile with types

--- src/StateManager/mainSlice.ts
+++ src/StateManager/mainSlice.ts
@@ -1,18 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 //Types
-type T_SideMenu = boolean 
+interface InterfaceState {
+    isMobile: boolean | null
+}
 
 //The slice
 export const mainSlice = createSlice({
     name: 'mainSlice',
     initialState:{
         isMobile: null
-    },
+    } as InterfaceState,
     reducers:{
         setIsMobile: (state, action) => {
-            const newState = action.payload
-            state.isMobile = newState
+            /* Reducer actions:
+            *  If the reducer payload is empty, it will toggle the state. 
+            *  Else, it will set a definitive value.
+            */
+            if(action.payload!=true || action.payload!=false){
+                state.isMobile
+                    ? state.isMobile = true
+                    : state.isMobile = false
+            }
+            else{
+                /*This means the there is a definitive state*/
+                const newState: boolean = action.payload
+                state.isMobile = newState
+            }
         }
     }
 })
