edccbc80d4101575d8cd1c0d7a0ed409674d8e55
Thenu Kaluarachchi <93025713+thenu-k@users.noreply.github.com>
2022-06-01 22:23:01 +0530

    Minor change to prevent merge conflict

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,11 +1,19 @@
 import { FC } from "react";
+import CreateTweet from "./CreateTweet/CreateTweet";
+import * as S from './Home.css'
 
 const Home: FC = () => {
   return (
-    <>
-      <p style={{ color: "white" }}>Home</p>
-    </>
-  );
+    // Parent Elements = MainSection -> Outlet(Auth) -> Content Section => Home
+    <S.HomeContainer>
+      <S.Title>
+        <h2>Home</h2>
+      </S.Title>
+      <S.HomeContent>
+        <CreateTweet/>
+      </S.HomeContent>
+    </S.HomeContainer>
+  )
 };
 
 export default Home;
